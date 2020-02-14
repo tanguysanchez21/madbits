13c58b92a6f24a519d316ea5e5adc03560df096e
tanguy21 <tanguysanchez21@gmail.com>
2020-02-14 23:32:05 +0100

    TRELLO - tasks : main character throw proj. [ 6 / 8 ]

--- Assets/Scripts/Attached/Hero.cs
+++ Assets/Scripts/Attached/Hero.cs
@@ -4,44 +4,38 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
-    private float spawnX;
-    private float spawnY;
-    private float spawnZ;
-
     public float baseATK;
     public float baseDEF;
-
     private float ATK;
     private float DEF;
 
-    private GameObject self;
-    private GameObject camera;
-
-    private float width;
-    private float height;
+    public Transform player;
+    public GameObject projectile;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         ATK = baseATK;
         DEF = baseDEF;
-
-        self = GameObject.Find("hero");
-        camera = GameObject.Find("camera");
-
-        width = self.transform.TransformPoint(0, 0, 0).x - self.transform.TransformPoint(1, 1, 0).x;
-        height = self.transform.TransformPoint(0, 0, 0).y - self.transform.TransformPoint(1, 1, 0).y;
-
-        spawnX = self.transform.position.x + ((width * 1)/2);
-        spawnY = self.transform.position.y;
-        spawnZ = self.transform.position.z;
-
-        self.transform.position = new Vector3(spawnX, spawnY, spawnZ);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //...
+        if (Input.GetKeyDown("space")) Fire();
+        if (Input.touchCount > 0) Fire();
+    }
+
+    void Fire() { animator.SetBool("AttackAnimation", true); }
+
+    void ThrowProjectile()
+    {
+        GameObject bullet = Instantiate(projectile) as GameObject;
+
+        Vector3 position = player.transform.position;
+        bullet.transform.position = new Vector3(position.x, position.y - 0.155f, position.z);
+
+        animator.SetBool("AttackAnimation", false);
     }
 }
---
+++ Assets/Scripts/Attached/Projectile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 0f;
+
+    private Rigidbody2D body;
+    //doesnt work
+    //private Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        body.velocity = new Vector2(speed, 0);
+
+        //doesnt work
+        //screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //doesnt work
+        //if(transform.position.x > screenBounds.x * -2) { Destroy(this.gameObject); }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "ennemy")
+        {
+            //debug only
+            //Destroy(other.gameObject);
+            other.gameObject.GetComponent<Animator>().SetBool("DamagedAnimation", true);
+            Destroy(this.gameObject);
+        }
+    }
+}
